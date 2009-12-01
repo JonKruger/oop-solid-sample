576f1061d6fceab3a806ed1c942640843904bc83
Jon Kruger <github@jonkruger.com>
2009-11-30 21:08:39 -0500

    added StructureMap and Rhino.Mocks

--- code/c-sharp/src/OopSolidSample/Global.asax.cs
+++ code/c-sharp/src/OopSolidSample/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using OopSolidSample.Config;
 
 namespace OopSolidSample
 {
@@ -26,6 +27,9 @@ namespace OopSolidSample
 
         protected void Application_Start()
         {
+            var config = new StandardStructureMapConfiguration();
+            config.Configure();
+
             RegisterRoutes(RouteTable.Routes);
         }
     }
