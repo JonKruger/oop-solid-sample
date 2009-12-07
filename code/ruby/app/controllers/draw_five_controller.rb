e6e407ad4b0a82b48747b084b8dec7330aa09d62
Jon Kruger <github@jonkruger.com>
2009-12-06 21:36:45 -0500

    working on porting DrawFive

---
+++ code/ruby/app/controllers/draw_five_controller.rb
@@ -0,0 +1,32 @@
+class DrawFiveController < ApplicationController
+  def index
+
+  end
+
+  def draw
+    game = DrawFiveGame.new
+    draw_result = game.draw
+
+    @cards = ''
+    draw_result.cards.each {|card|
+      if @cards.length > 0
+        @cards += ', '
+      end
+      @cards += card.description
+    }
+
+    @score = draw_result.score
+    @name = ''
+  end
+
+  def highscores
+    game = DrawFiveGame.new
+    @high_scores = game.get_high_scores
+  end
+
+  def savescore
+    game = DrawFiveGame.new
+    game.save_score(params[:name], params[:score])
+    redirect_to url_for :action => 'index'
+  end
+end
---
+++ code/ruby/test/unit/helpers/draw_five_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class DrawFiveHelperTest < ActionView::TestCase
+end
